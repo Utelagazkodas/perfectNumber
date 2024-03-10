4cf6fa422da9e6ccd787958632c21eae76a877d3
utelagazkodas <vargamiki53@gmail.com>
2024-03-10 20:08:25 +0100

    asd

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,19 @@ fn main() {
     let ONE : BigUint = (1 as u32).to_biguint().unwrap();
     let TWO : BigUint = (2 as u32).to_biguint().unwrap();
 
+    
+
     let mut p : u32 = 1;
     let mut number : BigUint;
-    while p < 16 {
-        number = pow(TWO.clone(), p.to_biguint().unwrap());
-
-        println!("{}",number.to_string().as_str());
+    while p < p + 1 {
+        number = pow(TWO.clone(), p.to_biguint().unwrap()) - ONE.clone();
 
         if is_prime(&(number.to_string().as_str())){
             println!("! WE GO(A)T A VALID P: {}", p);
             //println!("{}", ((2 as u128).pow(p) - 1) * (2 as u128).pow(p -1))
         }
         else {
-            println!("{} as p is invalid", p);
+         //   println!("{} as p is invalid", p);
         }
         p+= 1;
     }
